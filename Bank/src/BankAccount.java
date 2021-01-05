ef1196a1a946b233b6f1825d1a4885a710b6b611
moroojF <62428559+moroojF@users.noreply.github.com>
2021-01-05 03:12:55 +0300

    Add the rest of required methods

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -4,8 +4,9 @@ public class BankAccount {
 	private String accountNum;
 	private double checkBalance;
 	private double saveBalance;
-	static int accountCount;
-	static int totalMoney;
+	
+	static int accountCount=0;
+	static double totalMoney=0;
 
 	private String createAccount() {
 	    Random random = new Random();
@@ -17,6 +18,8 @@ public class BankAccount {
 
 	public BankAccount() {
 		this.accountNum = createAccount();
+		this.checkBalance=0;
+		this.saveBalance=0;
 		BankAccount.accountCount++;
 	}
 
@@ -26,6 +29,47 @@ public class BankAccount {
 
 	public double getSaveBalance() {
 		return saveBalance;
+	}
+	
+	public void depositMoney(double money, String type) {
+		if(type.equals("savings")) {
+			this.saveBalance += money;
+			}
+		else if(type.equals("checking")) {
+			this.checkBalance += money;
+			}
+		BankAccount.totalMoney += money;
+	}
+
+	public void withdrawMoney(double money, String type) {
+		if(type.equals("savings")){
+			if(this.saveBalance - money>=0){
+				this.saveBalance -= money;
+				BankAccount.totalMoney -= money;
+			}
+		}
+		else if(type.equals("checking")){
+			if(this.checkBalance-money>=0) {
+				this.checkBalance-=money;
+				BankAccount.totalMoney-=money;
+			}
+		}
+	}
+
+	public void displayAccountBalance() {
+		System.out.println(String.format("Savings: %.2f, Checking: %.2f", this.saveBalance, this.checkBalance));
+	}
+
+	public static void main(String[] args) {
+		BankAccount a = new BankAccount();
+		a.depositMoney(500.18, "checking");
+		a.depositMoney(362.9, "savings");
+		a.displayAccountBalance();
+		System.out.println(String.format("%.3f", BankAccount.totalMoney));
+		a.withdrawMoney(73.5, "checking");
+		a.withdrawMoney(10.3, "savings");
+		a.displayAccountBalance();
+		System.out.println(String.format("%.3f", BankAccount.totalMoney));
 	}
 
 }
